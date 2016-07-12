010c25907930b5afc31890c564ec853910cec988
izestrea <izestrea@gmail.com>
2016-07-12 19:15:12 +0300

    Practice exercise to handle form with xpath

--- src/main/java/InvokeBrowser.java
+++ src/main/java/InvokeBrowser.java
@@ -10,22 +10,28 @@ public class InvokeBrowser {
         System.setProperty("webdriver.chrome.driver", "D:\\apps\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
+        driver.get("http://techno-geek.co.uk/SeleniumPractice/Form2.html");
 //        driver.get("http://techno-geek.co.uk/SeleniumPractice/Form.html");
-        driver.get("http://techno-geek.co.uk/callmycab/");
+//        driver.get("http://techno-geek.co.uk/callmycab/");
         driver.manage().window().maximize();
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
 //        System.out.println(driver.getPageSource());
-        driver.findElement(By.xpath(".//*[@id='navbar']/ul/li[1]/a/nav/span[2]")).click();
-        driver.findElement(By.xpath(".//*[@id='development']")).click();
-        driver.findElement(By.xpath(".//*[@id='development']")).click();
-        System.out.println(driver.findElement(By.xpath(".//*[@id='development']")).isSelected());
-        driver.findElement(By.xpath(".//*[@id='under_13']")).click();
+//        driver.findElement(By.xpath(".//*[@id='navbar']/ul/li[1]/a/nav/span[2]")).click();
+//        driver.findElement(By.xpath(".//*[@id='development']")).click();
+//        driver.findElement(By.xpath(".//*[@id='development']")).click();
+//        System.out.println(driver.findElement(By.xpath(".//*[@id='development']")).isSelected());
+//        driver.findElement(By.xpath(".//*[@id='under_13']")).click();
 
-//        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("test@test.test");
-//        driver.findElement(By.xpath("//input[@id='name']")).sendKeys("Testname");
-//        driver.findElement(By.xpath("//input[@id='address']")).sendKeys("Some address");
-//        driver.findElement(By.xpath("//input[@id='hobby']")).sendKeys("Some hobby");
+
+        driver.findElement(By.xpath(".//*[@id='username']")).sendKeys("Testusername");
+        driver.findElement(By.xpath(".//*[@id='password']")).sendKeys("Testpassword");
+        driver.findElement(By.xpath(".//*[@id='name']")).sendKeys("Testname");
+        driver.findElement(By.xpath(".//*[@id='address']")).sendKeys("Some address");
+        driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("test@test.test");
+        driver.findElement(By.xpath(".//*[@id='Address']")).sendKeys("Some hobby");
+        driver.findElement(By.xpath(".//*[@id='postal']")).sendKeys("Some Profession");
+
 
 //        driver.quit();
     }
