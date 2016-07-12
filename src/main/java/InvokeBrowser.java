4a805b41dbe2a439e82607cf3d956513e267c000
izestrea <izestrea@gmail.com>
2016-07-12 16:53:12 +0300

    Add some driver operations

--- src/main/java/InvokeBrowser.java
+++ src/main/java/InvokeBrowser.java
@@ -9,8 +9,11 @@ public class InvokeBrowser {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","D:\\apps\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://login.yahoo.com/");
+        driver.get("http://techno-geek.co.uk/");
         driver.manage().window().maximize();
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getPageSource());
         driver.quit();
     }
 }
