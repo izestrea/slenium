77f31660e432da353b6faa3b921a1d933a0e2eb8
izestrea <izestrea@gmail.com>
2016-07-12 18:00:37 +0300

    Extract page element with xpath

--- src/main/java/InvokeBrowser.java
+++ src/main/java/InvokeBrowser.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -9,11 +10,14 @@ public class InvokeBrowser {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","D:\\apps\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("http://techno-geek.co.uk/");
+
+        driver.get("http://techno-geek.co.uk/callmycab/");
         driver.manage().window().maximize();
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
-        System.out.println(driver.getPageSource());
+//        System.out.println(driver.getPageSource());
+        System.out.println(driver.findElement(By.xpath(".//*[@id='navbar']/ul/li[1]/a/nav/span[2]")));
+
         driver.quit();
     }
 }
