c66b30922f3204688de7909bbeb57a2b3ddded42
izestrea <izestrea@gmail.com>
2016-07-12 18:43:18 +0300

    Extract form elements with xpath

--- src/main/java/InvokeBrowser.java
+++ src/main/java/InvokeBrowser.java
@@ -1,23 +1,27 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Created on 0012, July, 12.
  */
 public class InvokeBrowser {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","D:\\apps\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:\\apps\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-        driver.get("http://techno-geek.co.uk/callmycab/");
+        driver.get("http://techno-geek.co.uk/SeleniumPractice/Form.html");
         driver.manage().window().maximize();
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
 //        System.out.println(driver.getPageSource());
-        System.out.println(driver.findElement(By.xpath(".//*[@id='navbar']/ul/li[1]/a/nav/span[2]")));
+//        driver.findElement(By.xpath(".//*[@id='navbar']/ul/li[1]/a/nav/span[2]")).click();
 
-        driver.quit();
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("test@test.test");
+        driver.findElement(By.xpath("//input[@id='name']")).sendKeys("Testname");
+        driver.findElement(By.xpath("//input[@id='address']")).sendKeys("Some address");
+        driver.findElement(By.xpath("//input[@id='hobby']")).sendKeys("Some hobby");
+
+//        driver.quit();
     }
 }
